86bc97ea7b845d9ec4a1f942b7c58f73ff235089
AdelMej <adelmejrissi@gmail.com>
2025-11-24 09:42:16 +0100

    feat: added a script that adds two floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" A module containing a function that handles float addition"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function that adds two float
+
+    Attributes:
+        a(float): a float
+        b(float): a float
+
+    Returns:
+        the result of a + b
+    """
+    return (float(a + b))
---
+++ python_variable_annotations/README.md
@@ -0,0 +1,5 @@
+# repository for learning python type safety
+
+- type
+- duck typing
+- casting
