9af29598bf25087c98f25201f9aae9812d1ead7f
AdelMej <adelmejrissi@gmail.com>
2025-11-24 11:07:54 +0100

    feat: added a script that sums up a list of mixed int and float values

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" a function that sum up float and ints """
+from typing import List
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    a function that sums a mixed list of int and floats
+
+    Attributes:
+        mxd_lst(list(union[int, float])): a list of int and float
+
+    Returns:
+        result(float): the sum of all the values in the list
+    """
+    result = 0.0
+    for value in mxd_lst:
+        result += value
+
+    return result
