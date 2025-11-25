9db6d448b73a241823c14c03bcb1855c8a7cf64b
AdelMej <adelmejrissi@gmail.com>
2025-11-25 10:18:17 +0100

    feat: added a function that launch n amount of coroutines of wait_random

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import asyncio
+import heapq
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """
+    a function that create n coroutines and returns
+    a sorted list of wait_random
+
+    Attributes:
+        n(int): the number of wait_random coroutine to run
+        max_delay(int): the maximum delay for each wait_random coroutines
+
+    Returns:
+        a sorted list of wait_random dealays sorted in ascending order
+    """
+    if n == 0:
+        return []
+
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*coroutines)
+
+    # creating a heap with resulting gather
+    heap = []
+    for value in result:
+        heapq.heappush(heap, value)
+
+    # sort array by popping items from the heap they come with smallest first
+    sorted_array = [heapq.heappop(heap) for _ in range(len(heap))]
+
+    return sorted_array
