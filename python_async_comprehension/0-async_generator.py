d0b8ca7571a5b72dc431f3d84472f019e76711ef
AdelMej <adelmejrissi@gmail.com>
2025-11-25 15:44:30 +0100

    feat: added a script that create an async generator that can be used to generate random values asynchorniously

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+a module containing a function that generate asynchronically
+random numbers 10 times
+"""
+import typing
+import random
+import asyncio
+
+
+async def async_generator() -> typing.AsyncGenerator[float, None]:
+    """
+    a function that generate an AsyncGenerator that returns 10 random values
+    and await 1 second
+
+    Returns:
+        an asyncGenerator that can be iterated
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
