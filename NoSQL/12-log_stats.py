dc010336027d3497021e3c0c961ed31e3c0b97ea
Adel <adelmejrissi@gmail.com>
2025-12-03 10:53:43 +0100

    feat: added a script that output logs metadatas seperated with number of methods used and number of time /status was hit

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" a file containing a python script that output stats about nginx dump"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """
+    main entry point for my log watching script
+    """
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    logs_collection = client.logs.nginx
+
+    print(str(logs_collection.count_documents({})) + " logs")
+    print("Methods:")
+    print(
+        "\tmethod GET: " +
+        str(logs_collection.count_documents({"method": "GET"}))
+    )
+    print(
+        "\tmethod POST: " +
+        str(logs_collection.count_documents({"method": "POST"}))
+    )
+    print(
+        "\tmethod PUT: " +
+        str(logs_collection.count_documents({"method": "PUT"}))
+    )
+    print(
+        "\tmethod PATCH: " +
+        str(logs_collection.count_documents({"method": "PATCH"}))
+    )
+    print(
+        "\tmethod DELETE: " +
+        str(logs_collection.count_documents({"method": "DELETE"}))
+    )
+    print(
+        str(
+            logs_collection.count_documents(
+                {
+                    "method": "GET",
+                    "path": "/status"
+                }
+            )
+        ) +
+        " status check",
+        end=""
+    )
