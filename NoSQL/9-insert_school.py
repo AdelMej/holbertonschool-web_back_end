2f8fb8b98f3879a0246638d9b20928474ff03daf
Adel <adelmejrissi@gmail.com>
2025-12-03 09:20:40 +0100

    feat: added docstrings

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-import pymongo
+""" a module containing a function that insert item into mango db school collection"""
 
 
 def insert_school(mongo_collection, **kwargs):
+    """
+    function that insert elements in a collection
+
+    Attribute:
+        mango_collection: the collection to insert into
+
+    Returns:
+        the id of the newly inserted item
+    """
     result = mongo_collection.insert_one(kwargs)
 
     return result.inserted_id
