489dc3cb8e8d72cc93bcbfe197e3225f7c8eaee4
AdelMej <adelmejrissi@gmail.com>
2025-12-09 11:13:49 +0100

    fix: removed method overriding check in consructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
     this._sqft = sqft;
   }
 
