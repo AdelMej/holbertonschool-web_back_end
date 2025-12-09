5f433911c8cac1051be2f846bc976b659b219c1c
AdelMej <adelmejrissi@gmail.com>
2025-12-09 10:38:26 +0100

    feat: added a class that fix an amount and it's currency also has a displayFullPrice method for displaying the full price

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,28 @@
+import Currency from './3-currency.js';
+
+export default class Pîcing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+}
