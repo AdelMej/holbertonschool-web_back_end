d1b0e257f124b451c9f4d9939cf726359072b125
AdelMej <adelmejrissi@gmail.com>
2025-12-16 09:43:55 +0100

    fix: now log at every call

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,13 @@
 export default async function handleResponseFromApi(promise) {
   try {
     await promise;
-    console.log('Got a response from the API');
     return {
       status: 200,
       body: 'success',
     };
   } catch (err) {
     return Error();
+  } finally {
+    console.log('Got a response from the API');
   }
 }
