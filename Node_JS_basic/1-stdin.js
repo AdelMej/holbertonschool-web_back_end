78e0c8c546fceb6af43f05a052412124b586479c
Adel <adelmejrissi@gmail.com>
2026-01-05 11:12:36 +0100

    fix: removed the logic for parsing the newline in the data event input

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (chunk) => {
-  const name = chunk.replace(/\n$/, '');
+  const name = chunk.trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
