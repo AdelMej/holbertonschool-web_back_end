81104ab7ff61d7ce9e1ef63b14ad130cc8728422
Adel <adelmejrissi@gmail.com>
2026-01-05 11:01:36 +0100

    feat: added encoding in utf8 removed extra new line in input and removed console.log entirely to have more control on output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (chunk) => {
-  console.log(`Your name is: ${chunk}`);
+  const name = chunk.replace(/\n$/, '');
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
 });
