825624349bb2c388900dd101e3a93f2b047f8f6c
Adel <adelmejrissi@gmail.com>
2026-01-07 09:14:22 +0100

    feat: made a node http server that listen on port 12345 and return the same message on any request

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+server.listen(1245, () => {
+});
